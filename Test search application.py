bf58d72379eea99583510cf4e8ff3e3547d437c9
pouriyaparsa <parsa3click@gmail.com>
2022-07-16 16:20:49 +0430

    search Test 16/07

---
+++ Test search application.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(executable_path = ChromeDriverManager().install())
+from selenium.webdriver.common.by import By
+import time
+
+driver.get("https://3click.com/")
+driver.find_elements(By.CLASS_NAME,'location-btn')[0].click()
+time.sleep(1)
+search = driver.find_element(By.ID,'input-150').send_keys("ist")
+time.sleep(1)
+result = driver.find_elements(By.CLASS_NAME,'mr-1')[1].click()
+ist = driver.find_elements(By.CLASS_NAME,'text-truncate')[0].text
+print(ist)
+assert ist == 'استانبول'
+time.sleep(5)
+
+
