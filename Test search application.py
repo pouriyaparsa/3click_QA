cdb0258e926b667bf98df4c25ac7a4a65ab47d4e
pouriyaparsa <parsa3click@gmail.com>
2022-07-19 19:07:09 +0430

    search Test 2  17/07

--- Test search application.py
+++ Test search application.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import re
+
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+
+
+def log_event(message, log_console=True, log_to_file=True):
+    if log_console:
+        print(message)
+
+    if log_to_file:
+        with open('search.txt', 'a', encoding='utf-8') as f:
+            f.write(message)
 
 
 class find_site:
@@ -12,26 +21,32 @@ class find_site:
     driver.get(URL_Base)
     driver.maximize_window()
 
-class import_text:
-    element_field = driver.find_elements(By.CLASS_NAME, 'location-btn')[0]
-    element_field.click()
-    time.sleep(2)
-    element_text = driver.find_element(By.ID, 'input-150')
-    Text = ('پار', 'ika', 'bkk','mhd','lax','thr','kih')
-    patterns_dict = {}
-    for t in Text:
-        patterns_dict[t] = []
-    for pattern in Text:
-        print(f'>> The searched word is equal to === {pattern}')
-        element_text.send_keys(pattern)
-        time.sleep(2)
-        element_result = driver.find_elements(By.CLASS_NAME, 'mr-1')
-        for result in range(1, len(element_result) - 1):
-            subject_element = element_result[result].text
-            does_contain = len(re.findall(pattern, subject_element)) > 0
-            patterns_dict[pattern].append(does_contain)
-            print(f'> {subject_element}: {does_contain}')
-            element_text.send_keys(Keys.CONTROL + "a")
-            element_text.send_keys(Keys.DELETE)
-    driver.quit()
 
+class Action:
+    origin_element = driver.find_elements(By.CLASS_NAME, 'location-btn')[0]
+    origin_element.click()
+    time.sleep(1.5)
+    Enter_text = driver.find_element(By.ID, 'input-150')
+    search_dict = {'ist': 'استانبول',
+                   'thr': 'تهران',
+                   'mhd': 'مشهد',
+                   'lax': 'لس آنجلس',
+                   'swa': 'صبیحا'
+                   }
+    for keyword in search_dict.keys():
+        log_event(f'       >>>> The searched word is equal to {keyword}', log_console=False, log_to_file=True)
+        Enter_text.send_keys(keyword)
+        time.sleep(2)
+        total_result = driver.find_elements(By.CLASS_NAME, 'v-item-group')
+        for result in range(len(total_result) - 1):
+            result_text = total_result[result].text
+            log_event(result_text)
+            if search_dict.get(keyword) in result_text:
+                log_event(search_dict.get(keyword))
+                log_event("وجود دارد")
+            else:
+                log_event(search_dict.get(keyword))
+                log_event('ندارد')
+        Enter_text.send_keys(Keys.CONTROL + "a")
+        Enter_text.send_keys(Keys.DELETE)
+    driver.quit()
